2fb55bc42af59f792a61a2215e654ae838a4c033
witkowskipiotr <witkowski_piotr@hotmail.com>
2017-10-26 12:19:07 +0200

    Front page HTML now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -12,22 +12,22 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        # request = HttpRequest()
-        # response = home_page(request)
-
-        response = self.client.get('/')
-        html = response.content.decode('utf-8')
-        # expected_html = render_to_string('home.html')
-        # self.assertEqual(html, expected_html)
-
-
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-
-        self.assertTemplateUsed(response, 'home.html')
+    # def test_root_url_resolves_to_home_page_view(self):
+    #     found = resolve('/')
+    #     self.assertEqual(found.func, home_page)
+    #
+    # def test_home_page_returns_correct_html(self):
+    #     # request = HttpRequest()
+    #     # response = home_page(request)
+    #
+    #     response = self.client.get('/')
+    #     html = response.content.decode('utf-8')
+    #     # expected_html = render_to_string('home.html')
+    #     # self.assertEqual(html, expected_html)
+    #
+    #
+    #     self.assertTrue(html.startswith('<html>'))
+    #     self.assertIn('<title>To-Do lists</title>', html)
+    #     self.assertTrue(html.strip().endswith('</html>'))
+    #
+    #     self.assertTemplateUsed(response, 'home.html')
