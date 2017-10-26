795fba681ec58b9a2272f7da8448bb67eb403828
witkowskipiotr <witkowski_piotr@hotmail.com>
2017-10-26 15:43:07 +0200

    next step

--- superlists/tests/functional_test.py
+++ superlists/tests/functional_test.py
@@ -11,7 +11,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_starting_a_a_new_todo_list(self):
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new to-do lists app.
         # She goes to its homepage
         self.browser.get('http://localhost:8000')
@@ -41,22 +46,20 @@ class NewVisitorTest(unittest.TestCase):
 
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table. Contents were:\n%s" % (
-             table.text,
-             )
-        )
+        self.check_for_row_in_list_table('Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
 
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
+
+        self.check_for_row_in_list_table('Buy peacock feathers')
+        self.check_for_row_in_list_table('Use peacock feathers to make a fly')
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
