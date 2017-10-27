89934a80c2dc5911067c8f2c2e06b8c5d6a4d635
witkowskipiotr <witkowski_piotr@hotmail.com>
2017-10-27 15:23:16 +0200

    add new users

--- lists/views.py
+++ lists/views.py
@@ -1,20 +1,12 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from lists.models import Item
 
 
 def home_page(request):
     if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-    else:
-        new_item_text = ''
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
 
-
-    return render(request, 'home.html', {
-        'new_item_text': new_item_text,
-    })
-
-    # if request.method == 'POST':
-    #     return HttpResponse(request.POST['item_text'])
-    # return render(request, 'home.html')
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
